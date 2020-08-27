12b0a58ebe89dd2f3168d6e6e234b36a9c340787
sharatchandra-ts <sharatchandrats@gmail.com>
2020-08-27 18:05:53 +0530

    not much changes

--- SharatAndroid/join/lib/main.dart
+++ SharatAndroid/join/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:join/pages/meetings.dart';
 
 void main() {
-  runApp(MaterialApp(
-    
-  ));
+  runApp(MaterialApp());
 }
---
+++ SharatAndroid/join/lib/pages/about.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) => runApp(Material());
+
+class About {}
+
+class AboutWindow extends StatefulWidget {
+  @override
+  _AboutWindowState createState() => _AboutWindowState();
+}
+
+class _AboutWindowState extends State<AboutWindow> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
