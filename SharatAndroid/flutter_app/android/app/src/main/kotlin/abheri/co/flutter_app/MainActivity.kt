bc5a38271410c514cb75ab6058417a584848ecfd
sharatchandra-ts <sharatchandrats@gmail.com>
2020-10-01 13:54:54 +0530

    Bug Fixes

---
+++ SharatAndroid/flutter_app/android/app/src/main/kotlin/abheri/co/flutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package abheri.co.flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ SharatAndroid/join/lib/styles/icons.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const topAdd = 'asset/topAdd.svg';
